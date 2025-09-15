10ca5408dd569e74b8e0a6436ef128376bc95568
NievaS24 <santinieva24@gmail.com>
2025-09-15 17:11:33 -0300

    Controlador creado + 2 HttpsGet.

---
+++ MiWebApi/Controllers/CadeteriaController.cs
@@ -0,0 +1,30 @@
+using MiWebApi.Models;
+using MiWebApi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CadeteriaController : ControllerBase
+    {
+        private AccesoADatos accesoADatos;
+        public CadeteriaController()
+        {
+            //esto es el constructor de la clase
+            accesoADatos = new AccesoADatos();
+        }
+
+        [HttpGet("Pedidos")]
+        public IActionResult GetPedidos()
+        {
+            var listadoPedidos = accesoADatos.GetPedidos();
+            return Ok(listadoPedidos);
+        }
+        [HttpGet("Cadetes")]
+        public IActionResult GetCadetes()
+        {
+            var listadoCadetes = accesoADatos.GetCadetes(); return Ok(listadoCadetes);
+        }
+    }
+}
